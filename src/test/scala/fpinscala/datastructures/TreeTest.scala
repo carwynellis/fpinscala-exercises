e2d858167e964455ff3c111ff0074b7c7a078c50
Carwyn Ellis <carwynellis@gmail.com>
2016-06-05 18:09:08 +0100

    Exercise 3.28 solution and test

--- src/test/scala/fpinscala/datastructures/TreeTest.scala
+++ src/test/scala/fpinscala/datastructures/TreeTest.scala
@@ -26,7 +26,7 @@ class TreeTest extends FunSuite {
 
   test("depth returns 0 for a tree composed of a single leaf") {
     val t = Leaf(0)
-    assertResult(1) { Tree.depth(t) }
+    assertResult(0) { Tree.depth(t) }
   }
 
   test("depth returns 1 for a tree with single branch with two leaf nodes") {
@@ -37,5 +37,17 @@ class TreeTest extends FunSuite {
   test("depth returns 3 for an asymmetric tree") {
     val t = Branch(Leaf(1), Branch(Leaf(2), Branch(Leaf(3), Leaf(4))))
     assertResult(3) { Tree.depth(t) }
+  }
+
+  test("map of a single leaf tree returns single leaf tree with function applied") {
+    val t = Leaf(1)
+    assertResult(Leaf(2)) { Tree.map(t)((i: Int) => i + 1) }
+  }
+
+  test("map of a more complex tree returns the same structure with modified values") {
+    val t = Branch(Leaf(1), Branch(Leaf(2), Branch(Leaf(3), Leaf(4))))
+    assertResult(Branch(Leaf(2), Branch(Leaf(3), Branch(Leaf(4), Leaf(5))))) {
+      Tree.map(t)((i: Int) => i + 1)
+    }
   }
 }
