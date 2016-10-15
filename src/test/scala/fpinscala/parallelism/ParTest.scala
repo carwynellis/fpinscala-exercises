7a59aee321c16384a352aab7c2581d462280ca85
Carwyn Ellis <carwynellis@gmail.com>
2016-10-15 18:06:45 +0100

    Tests for map2 and TimeoutFuture behaviour

--- src/test/scala/fpinscala/parallelism/ParTest.scala
+++ src/test/scala/fpinscala/parallelism/ParTest.scala
@@ -1,13 +1,38 @@
 package fpinscala.parallelism
 
-import java.util.concurrent.{Executors, TimeUnit}
+import java.util.concurrent.{Executors, TimeUnit, Future}
 
 import fpinscala.parallelism.Par.Par
 import org.scalatest.{AsyncFunSuite, Matchers}
 
+import scala.concurrent.TimeoutException
+
 class ParTest extends AsyncFunSuite with Matchers {
 
   private val executorService = Executors.newWorkStealingPool()
+
+  // Note that the map2 tests implicitly test the TimeoutFuture.
+  test("map2 applies the specified function to the specified pars") {
+    val par = Par.unit[Int](1)
+
+    val result = Par.run(executorService)(Par.map2(par,par)(_ + _))
+
+    result.get(1, TimeUnit.SECONDS) should be (2)
+  }
+
+  test("map2 throws TimeoutException if futures don't complete in time") {
+    val par = Par.lazyUnit[Int] {
+      Thread.sleep(50)
+      1
+    }
+
+    val result = Par.run(executorService)(Par.map2(par,par)(_ + _))
+
+    a[TimeoutException] should be thrownBy result.get(
+      5,
+      TimeUnit.NANOSECONDS
+    )
+  }
 
   test("asyncF evaluates specified function asynchronously") {
     val asyncF: (Int) => Par[Int] = Par.asyncF[Int, Int](_ + 1)
@@ -16,5 +41,6 @@ class ParTest extends AsyncFunSuite with Matchers {
 
     f.get(1, TimeUnit.SECONDS) should be(2)
   }
+
 
 }
