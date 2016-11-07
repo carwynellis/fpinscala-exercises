0ad9b313e6d475879c33b59ab79a3be1d92c4cf7
Carwyn Ellis <carwynellis@gmail.com>
2016-11-07 17:21:43 +0000

    Added forAll implementation and associated types.
    The book walks through the development of the api and these changes are
    required to continue with the exercises.

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -1,6 +1,7 @@
 package fpinscala.testing
 
 import fpinscala.state._
+import fpinscala.laziness.Stream
 import fpinscala.testing.Prop._
 
 /*
@@ -8,15 +9,43 @@ The library developed in this chapter goes through several iterations. This file
 shell, which you can fill in and modify while working through the chapter.
 */
 
-trait Prop {
-  def check: Either[(FailedCase,SuccessCount), SuccessCount]
-}
+case class Prop(run: (TestCases, RNG) => Result)
 
 object Prop {
   type FailedCase = String
   type SuccessCount = Int
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  type TestCases = Int
+
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+
+  case object Passed extends Result {
+    def isFalsified = false
+  }
+
+  case class Falsified(failure: FailedCase,
+                       successes: SuccessCount) extends Result {
+    def isFalsified = true
+  }
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n,rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace: \n ${e.getStackTrace.mkString("\n")}"
 }
+
 
 object Gen {
   def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
