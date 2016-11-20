42faf67cc52158227279017d6e891a7c324a2065
Carwyn Ellis <carwynellis@gmail.com>
2016-11-20 17:27:00 +0000

    Added code covered in chapter 9 introduction

--- src/main/scala/fpinscala/parsing/Parsers.scala
+++ src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,15 +1,58 @@
 package fpinscala.parsing
 
+import fpinscala.testing.{Gen, Prop}
+import fpinscala.testing.Prop._
+
 import language.higherKinds
+import language.implicitConversions
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def many1[A](p: Parser[A]): Parser[List[A]]
+
+  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)]
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]):ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
   case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
+    // TODO - clarify why we pass by name in this version of or
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
+    def many = self.many(p)
+
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+
+    def **[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
+
+    def product[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
   }
 
   object Laws {
+
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in, "equal")(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(identity))(in)
+
   }
 }
 
