dc59e4b407b65eb0ef2b5218876c812d2752ce83
Carwyn Ellis <carwynellis@gmail.com>
2016-12-18 18:00:45 +0000

    Exercise 10.1 solution and tests

--- src/test/scala/fpinscala/monoids/MonoidTest.scala
+++ src/test/scala/fpinscala/monoids/MonoidTest.scala
@@ -12,4 +12,20 @@ class MonoidTest extends FunSuite with Matchers {
     Monoid.listMonoid.op(List(1,2), List(3,4)) should be(List(1,2,3,4))
   }
 
+  test("intAddition monoid adds two ints correctly") {
+    Monoid.intAddition.op(1, 2) should be(3)
+  }
+
+  test("intMultiplication monoid adds two ints correctly") {
+    Monoid.intMultiplication.op(1, 2) should be(2)
+  }
+
+  test("booleanOr monoid combines two booleans correctly") {
+    Monoid.booleanOr.op(true, false) should be(true)
+  }
+
+  test("booleanAnd monoid combines two booleans correctly") {
+    Monoid.booleanAnd.op(true, false) should be(false)
+  }
+
 }
