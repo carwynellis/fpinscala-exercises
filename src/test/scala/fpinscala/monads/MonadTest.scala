058cb04db868f0a3d20d24118cb687eb79abd3fb
Carwyn Ellis <carwynellis@gmail.com>
2017-01-02 19:12:15 +0000

    Exercise 11.8 solution and test

--- src/test/scala/fpinscala/monads/MonadTest.scala
+++ src/test/scala/fpinscala/monads/MonadTest.scala
@@ -5,57 +5,51 @@ import org.scalatest.{FunSuite, Matchers}
 class MonadTest extends FunSuite with Matchers {
 
   test("sequence returns expected result for list monad") {
-    val lm = Monad.listMonad
-
     val l = List(List(1), List(2), List(3))
 
-    lm.sequence(l) should be(List(List(1,2,3)))
+    Monad.listMonad.sequence(l) should be(List(List(1,2,3)))
   }
 
   test("traverse returns expected result for list monad") {
-    val lm = Monad.listMonad
-
     val l = List(List(1), List(2), List(3))
 
     val f = (l: List[Int]) => l.map(_ + 1)
 
-    lm.traverse(l)(f) should be(List(List(2,3,4)))
+    Monad.listMonad.traverse(l)(f) should be(List(List(2,3,4)))
   }
 
   test("replicateM returns expected result for list monad") {
-    val lm = Monad.listMonad
-
     val l = List(1)
 
-    lm.replicateM(3, l) should be(List(List(1, 1, 1)))
+    Monad.listMonad.replicateM(3, l) should be(List(List(1, 1, 1)))
   }
 
   test("replicateM returns expected result for option monad") {
-    val om = Monad.optionMonad
-
     val o = Some(1)
 
-    om.replicateM(3, o) should be(Some(List(1, 1, 1)))
+    Monad.optionMonad.replicateM(3, o) should be(Some(List(1, 1, 1)))
   }
 
   test("filterM returns expected result for list monad") {
-    val lm = Monad.listMonad
-
     val l = List(1,2,3,4,5)
 
-    val result = lm.filterM(l) { (i: Int) => lm.unit(i % 2 == 0) }
+    val result = Monad.listMonad.filterM(l) { (i: Int) =>
+      Monad.listMonad.unit(i % 2 == 0)
+    }
 
     result should be(List(List(2,4)))
   }
 
   test("compose composes two kleisli arrows correctly for option monad") {
-    val om = Monad.optionMonad
-
     val f = (i: Int) => Some(i + 1)
     val g = (i: Int) => Some(i * 2)
 
-    om.compose(f, g)(1) should be(Some(4))
-
+    Monad.optionMonad.compose(f, g)(1) should be(Some(4))
   }
 
+  test("_flatMap returns expected result for list monad") {
+    val l = List(1,2,3)
+
+    Monad.listMonad.flatMap(l)((i: Int) => List(i + 1)) should be(List(2,3,4))
+  }
 }
