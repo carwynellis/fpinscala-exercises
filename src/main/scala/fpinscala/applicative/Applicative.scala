9e1660ed1f06cd9286f1356c2e0d5f29abb84209
Carwyn Ellis <carwynellis@gmail.com>
2017-01-07 18:19:48 +0000

    Exercise 12.3 solution

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -19,7 +19,37 @@ trait Applicative[F[_]] extends Functor[F] {
   def map[A,B](fa: F[A])(f: A => B): F[B] = apply[A,B](unit(f))(fa)
 
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
-    apply[B,C](map(fa)(f.curried))(fb)
+    apply(
+      apply(
+        unit(f.curried)
+      )(fa)
+    )(fb)
+
+  def map3[A,B,C,D](fa: F[A],
+                    fb: F[B],
+                    fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(
+      apply(
+        apply(
+          unit(f.curried)
+        )(fa)
+      )(fb)
+    )(fc)
+
+  def map4[A,B,C,D,E](fa: F[A],
+                      fb: F[B],
+                      fc: F[C],
+                      fd: F[D])(f: (A, B, C, D) => E): F[E] =
+    apply(
+      apply(
+        apply(
+          apply(
+            unit(f.curried)
+          )(fa)
+        )(fb)
+      )(fc)
+    )(fd)
+
 
   def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List.empty[B])) { (elem, acc) =>
