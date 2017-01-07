0c42301830f83b7f3a9cb8c2fc59d921b19efa12
Carwyn Ellis <carwynellis@gmail.com>
2017-01-07 18:02:35 +0000

    Exercise 12.2 solution

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -11,14 +11,15 @@ import language.implicitConversions
 
 trait Applicative[F[_]] extends Functor[F] {
 
-  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa) { (f,v) => f(v) }
 
   def unit[A](a: => A): F[A]
 
-  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = ???
+  def map[A,B](fa: F[A])(f: A => B): F[B] = apply[A,B](unit(f))(fa)
 
-  def map[A,B](fa: F[A])(f: A => B): F[B] =
-    map2(fa, unit(()))((a,_) => f(a))
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply[B,C](map(fa)(f.curried))(fb)
 
   def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List.empty[B])) { (elem, acc) =>
