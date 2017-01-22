56d7024e683445a8255bd31f1142d37f7e0b1800
Carwyn Ellis <carwynellis@gmail.com>
2017-01-22 18:47:13 +0000

    Exercise 13.2 solution

--- src/main/scala/fpinscala/iomonad/IO.scala
+++ src/main/scala/fpinscala/iomonad/IO.scala
@@ -384,7 +384,15 @@ object IO3 {
 
   // Exercise 2: Implement a specialized `Function0` interpreter.
   // @annotation.tailrec
-  def runTrampoline[A](a: Free[Function0,A]): A = ???
+  def runTrampoline[A](a: Free[Function0,A]): A = a match {
+    case Return(v)     => v
+    case Suspend(s)    => s()
+    case FlatMap(s, f) => s match {
+      case Return(v)     => runTrampoline(f(v))
+      case Suspend(g)    => runTrampoline(f(g))
+      case FlatMap(t, g) => runTrampoline(t flatMap (x => g(x) flatMap f))
+    }
+  }
 
   // Exercise 3: Implement a `Free` interpreter which works for any `Monad`
   def run[F[_],A](a: Free[F,A])(implicit F: Monad[F]): F[A] = ???
